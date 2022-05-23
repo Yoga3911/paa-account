593e5536cf56a7437f2ea5cd8b62ac7c8dbd6db7
Robin Hood <yoga3911@gmail.com>
2022-05-23 21:10:57 +0700

    init

--- app/api/api.js
+++ app/api/api.js
@@ -1,44 +1,47 @@
 const express = require("express")
+const jwt = require("jsonwebtoken")
 const router = express.Router()
-// const User = require("../models/User")
-// const Admin = require("../models/Admin")
-// const Req_to_be_seller = require("../models/Req_to_be_seller")
-const Product = require("../models/Product")
-const Category = require("../models/Category")
-const Review = require("../models/Review")
-const RPhoto = require("../models/RPhoto")
-const ReviewRPhoto = require("../models/Review_RPhoto")
-const Acc_Product = require("../models/Acc_Product")
+const User = require("../models/User")
+const Admin = require("../models/Admin")
+const Req_to_be_seller = require("../models/Req_to_be_seller")
 
-router.get("/users", async (_, res) => {
+router.post("/users/login", async (req, res) => {
+    const data = req.body
     try {
-        await User.findAll({
-            order: [
-                ["user_id", "ASC"]
-            ]
-        }).then((users) => res.status(200).json({
-            status: true,
-            message: "Get all user successfully",
-            data: users
-        }))
-    } catch (e) {
-        res.status(500).json({
+        const result = await User.findOne({
+            where: {
+                email: data.email,
+                password: data.password,
+            }
+        })
+
+        if (result != null) {
+            const secret = process.env.JWT_SECRET
+            const expire = "1h"
+            const data = {
+                id: result.user_id,
+                username: result.username,
+                email: result.email,
+                password: result.password,
+                address: result.address,
+            }
+
+            const token = jwt.sign(data, secret, { algorithm: "HS256", expiresIn: expire })
+
+            res.status(200).json({
+                status: true,
+                message: "Login berhasil!",
+                data: result,
+                token: token,
+            })
+            return;
+        }
+
+        res.status(404).json({
             status: false,
-            message: e,
+            message: "User not found!",
             data: null
         })
-    }
-})
-
-router.post("/review", async (req, res) => {
-    try {
-        await Review.create(
-            req.body
-        ).then((review) => res.status(201).json({
-            status: true,
-            message: "Insert review successfully",
-            data: review
-        }))
 
     } catch (e) {
         res.status(500).json({
